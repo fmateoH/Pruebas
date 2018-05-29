91091ac103951b34181ed61a9e452398a35f0e93
fmateoH <fmateo@ikeasistencia.com>
2018-05-29 13:24:31 -0500

    Commit pruba2

--- app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
@@ -32,14 +32,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     Button btnValidar;
-    EditText txtCraneId, txtDeviceid, code, device;
+    EditText code, device;
     TextView textResultado;
     Validate validacion;
     RespuestaJson respuesta;
 
-   // private String streamdataioProxyPrefix = "https://streamdata.motwin.net/";
-   Button btnValidar3;
-   Button btnValidar2;
+
     private String streamdataioAppToken = "YOUR_TOKEN_HERE";
 
     private String myApiUrl = "YOUR_JSON_API_URL";
