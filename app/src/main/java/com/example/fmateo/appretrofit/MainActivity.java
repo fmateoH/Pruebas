6dc7bbb4d94c90085aca1538939301363258f666
Carlos Carrillo <ccarrillo@ikeasistencia.com>
2018-05-31 11:53:59 -0500

    Coppel: Usted debe $ 5,000,000.00; Pase a pagar por favor.

--- app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Tokenizar> call, Throwable t) {
-            	//Comentario commit numero siete
+            	//Comentario commit numero ocho
 
             }
 
