024cb9ab39c4a54339f412b7dcc46461b02d7b42
Carlos Carrillo <ccarrillo@ikeasistencia.com>
2018-05-31 11:41:06 -0500

    Mi mensaje

--- app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Tokenizar> call, Throwable t) {
-            	//Comentario commit
+            	//Comentario commit numero 2
 
             }
 
