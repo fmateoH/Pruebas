6fdb8be3cf91fd638856bf2fba6da92b8aefd2f3
Carlos Carrillo <ccarrillo@ikeasistencia.com>
2018-05-31 12:18:39 -0500

    Amazon: Carlos Carrillo lo ha liberado de su deuda. puede dormir tranquilo.

--- app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Tokenizar> call, Throwable t) {
-            	//Comentario commit numero once
+            	//Comentario commit numero doce
             }
 
         });
