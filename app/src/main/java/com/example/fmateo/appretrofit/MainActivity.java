d82ec8415b573a9e892a48c27673bf3c54e522ca
Carlos Carrillo <ccarrillo@ikeasistencia.com>
2018-05-31 13:08:33 -0500

    Amazon y Coppel 4 La revancha 2

--- app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Tokenizar> call, Throwable t) {
-            	//Comentario commit numero dieciocho 18
+            	//Comentario commit numero diecinueve
             }
 
         });
