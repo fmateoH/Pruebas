ca042d4bcfacc662fd0de85b36c243e6e53026c5
Carlos Carrillo <ccarrillo@ikeasistencia.com>
2018-05-31 12:56:57 -0500

    Amazon y Coppel

--- app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/fmateo/appretrofit/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Tokenizar> call, Throwable t) {
-            	//Comentario commit numero quince 15
+            	//Comentario commit numero dieciseis 16
             }
 
         });
